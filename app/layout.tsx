0be619e1a06ee2b4714bd8d09fa5717b33927248
NiceZGT730 <ngamemax@gmail.com>
2025-04-09 15:29:20 +0700

    edit layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,34 @@ import type { ReactNode } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./tailwind.css";
+import Head from "next/head";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <title>Fullstack Cyberpunk Portfolio</title>
+        <title>Portfolio Bandit Chaluay</title>
         <meta
           name="description"
           content="Cyberpunk-themed portfolio of a Fullstack Developer"
         />
+        <meta property="og:title" content="Portfolio Bandit Chaluay" />
+        <meta
+          property="og:description"
+          content="Cyberpunk-themed portfolio of a Fullstack Developer"
+        />
+        <meta
+          property="og:image"
+          content="https://img2.pic.in.th/pic/port.md.jpg" 
+        />
+        <meta property="og:url" content="URL ของหน้าเว็บ" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Portfolio Bandit Chaluay" />
+        <meta
+          name="twitter:description"
+          content="Cyberpunk-themed portfolio of a Fullstack Developer"
+        />
+        <meta name="twitter:image" content="https://img2.pic.in.th/pic/port.md.jpg" />
         <link
           href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
